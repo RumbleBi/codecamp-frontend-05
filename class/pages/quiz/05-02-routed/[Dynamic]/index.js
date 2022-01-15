12a83ab21fa0a336588cb4f7f61e858da6941cd7
RumbleBi <jo@JOui-MacBookAir.local>
2022-01-16 00:06:09 +0900

    quizUpload

--- class/pages/quiz/05-02-routed/[Dynamic]/index.js
+++ class/pages/quiz/05-02-routed/[Dynamic]/index.js
@@ -1,34 +1,31 @@
-import { useRouter } from "next/router"
-import { useQuery, gql } from "@apollo/client"
+import { useRouter } from "next/router";
+import { useQuery, gql } from "@apollo/client";
 
 const FETCH_PRODUCT = gql`
-    query fetchProduct($productId: ID) {
-        fetchProduct(productId: $productId) {
-            name
-            detail
-            price
-            seller
-        }
+  query fetchProduct($productId: ID) {
+    fetchProduct(productId: $productId) {
+      seller
+      name
+      detail
+      price
     }
-`
+  }
+`;
 
 export default function DynamicRoutedPage() {
-    
-    const router = useRouter()
-    const { data } = useQuery(FETCH_PRODUCT, {
-        variables: { producId: router.query.Dynamic}
-    })
+  const router = useRouter();
+  const { data } = useQuery(FETCH_PRODUCT, {
+    variables: { productId: router.query.Dynamic },
+  });
+  console.log(data);
 
-    console.log(data)
-
-    return (
-        <>
-            <div>{router.query.Dynamic}번 게시글 페이지 이동 완료</div>
-            {/* <div>판매자: {data?.fetchProduct.seller}</div> */}
-            <div>상품명: {data?.fetchProduct.name}</div>
-            <div>상세내용: {data && data.fetchProduct?.detail}</div>
-            {/* detail이 있으면 찍고, 없으면 안찍는 것. 게시글 삭제같은걸로 오류가 일어나는걸 방지 */}
-            <div>상품가격: {data && data.fetchProduct.price}</div>
-        </>
-    )
+  return (
+    <>
+      <div>{router.query.Dynamic}번 게시글 페이지 이동 완료</div>
+      <div>판매자: {data?.fetchProduct.seller}</div>
+      <div>상품명: {data?.fetchProduct.name}</div>
+      <div>상세내용: {data && data.fetchProduct?.detail}</div>
+      <div>상품가격: {data && data.fetchProduct.price}</div>
+    </>
+  );
 }
