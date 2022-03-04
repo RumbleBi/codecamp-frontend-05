423751eb4e5e7a855ad76f89f649a7bfab880884
RumbleBi <olzlel2000@naver.com>
2022-03-04 11:11:44 +0900

    TIL

---
+++ class_build/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true, // 우리가 만든 폴더구조처럼 out폴더안에 생성
+};
+
+module.exports = nextConfig;
---
+++ class_build/pages/boards/index.js
@@ -0,0 +1,7 @@
+export default function BoardsPage() {
+  return (
+    <div>
+      <div>안녕하세요! 게시판입니다!</div>
+    </div>
+  );
+}
