ebf490f0cd9b7cf2f61055c259e93831b3174606
RumbleBi <olzlel2000@naver.com>
2022-03-05 18:27:52 +0900

    AddToWordle

--- freeboard_frontend/src/components/units/wordle/Wordle.container.tsx
+++ freeboard_frontend/src/components/units/wordle/Wordle.container.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import WordlePageUI from "./Wordle.presenter";
 
 export default function Wordle() {
@@ -14,64 +14,74 @@ export default function Wordle() {
   const [isCorrect4, setIsCorrect4] = useState(0);
 
   const inputAnswer = "abcde";
-  const onChangeInput0 = (event) => {
-    setInput0(event.target.value);
+  const onChangeInput0 = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput0(event.currentTarget.value);
   };
-  const onChangeInput1 = (event) => {
-    setInput1(event.target.value);
+  const onChangeInput1 = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput1(event.currentTarget.value);
   };
-  const onChangeInput2 = (event) => {
-    setInput2(event.target.value);
+  const onChangeInput2 = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput2(event.currentTarget.value);
   };
-  const onChangeInput3 = (event) => {
-    setInput3(event.target.value);
+  const onChangeInput3 = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput3(event.currentTarget.value);
   };
-  const onChangeInput4 = (event) => {
-    setInput4(event.target.value);
+  const onChangeInput4 = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput4(event.currentTarget.value);
   };
-  console.log(input0);
-  console.log(input1);
-  console.log(input2);
-  console.log(input3);
-  console.log(input4);
+  // console.log(input0);
+  // console.log(input1);
+  // console.log(input2);
+  // console.log(input3);
+  // console.log(input4);
 
   const onClickSubmit = () => {
     if (inputAnswer[0] === input0) {
       setIsCorrect0(1);
-    } else if (inputAnswer[0].includes(input0)) {
+    } else if (inputAnswer.includes(input0) === true) {
       setIsCorrect0(2);
     } else {
       setIsCorrect0(3);
     }
+    console.log(inputAnswer.includes(input0));
 
     if (inputAnswer[1] === input1) {
       setIsCorrect1(1);
-    } else if (inputAnswer[1].includes(input1)) {
+    } else if (inputAnswer.includes(input1) === true) {
       setIsCorrect1(2);
     } else {
       setIsCorrect1(3);
     }
     if (inputAnswer[2] === input2) {
       setIsCorrect2(1);
-    } else if (inputAnswer[2].includes(input2)) {
+    } else if (inputAnswer.includes(input2) === true) {
       setIsCorrect2(2);
     } else {
       setIsCorrect2(3);
     }
     if (inputAnswer[3] === input3) {
       setIsCorrect3(1);
-    } else if (inputAnswer[3].includes(input3)) {
+    } else if (inputAnswer.includes(input3) === true) {
       setIsCorrect3(2);
     } else {
       setIsCorrect3(3);
     }
     if (inputAnswer[4] === input4) {
       setIsCorrect4(1);
-    } else if (inputAnswer[4].includes(input4)) {
+    } else if (inputAnswer.includes(input4) === true) {
       setIsCorrect4(2);
     } else {
       setIsCorrect4(3);
     }
+
+    if (
+      isCorrect0 === 1 &&
+      isCorrect1 === 1 &&
+      isCorrect2 === 1 &&
+      isCorrect3 === 1 &&
+      isCorrect4 === 1
+    )
+      alert("정답입니다!!");
   };
 
   return (
