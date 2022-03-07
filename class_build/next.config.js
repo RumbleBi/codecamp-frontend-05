d39876710ff656e859fa2c79b31da223520908ee
RumbleBi <olzlel2000@naver.com>
2022-03-07 13:21:45 +0900

    TIL:SSR

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true, // 우리가 만든 폴더구조처럼 out폴더안에 생성
+  generateBuildId: () => "king-nyaa-deploy",
+  exportPathMap: () => ({
+    "/": { page: "/boards" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
