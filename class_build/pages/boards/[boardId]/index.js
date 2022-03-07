ff0b8619aeb1e225cf1e88a1948463f1cdf9ddc6
RumbleBi <olzlel2000@naver.com>
2022-03-07 12:28:46 +0900

    update

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,6 +1,15 @@
 import { useRouter } from "next/router";
-
+import Head from "next/head";
 export default function BoardsDetailPage() {
   const router = useRouter();
-  return <div>안녕하세요! 게시글 상세페이지 입니다!!, 게시글ID는{router.query.boardId}입니다!</div>;
+  return (
+    <div>
+      <Head>
+        <meta property="og:title" content="나의 게시판입니다" />
+        <meta property="og:description" content="저의 게시판에 오신것을 환영합니다" />
+        <meta property="og:image" content="/image/hugme.png" />
+      </Head>
+      <div>안녕하세요! 게시글 상세페이지 입니다!!, 게시글ID는{router.query.boardId}입니다!</div>
+    </div>
+  );
 }
