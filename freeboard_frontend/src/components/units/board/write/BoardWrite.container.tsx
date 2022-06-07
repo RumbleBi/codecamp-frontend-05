b12acfe21db881e11000a1fb4b37f6f9cabbd9e0
RumbleBi <olzlel2000@gmail.com>
2022-06-07 19:53:15 +0900

    refactoring

--- freeboard_frontend/src/components/units/board/write/BoardWrite.container.tsx
+++ freeboard_frontend/src/components/units/board/write/BoardWrite.container.tsx
@@ -142,19 +142,19 @@ export default function BoardWrite(props: IBoardWriteProps) {
   const onClickSubmit = async () => {
     if (writer === '') {
       setWriterError('작성자를 입력해주세요.')
-      window.scrollTo(100, 500)
+      window.scrollTo(0, 0)
     }
     if (password.length > 16 || password.length < 8) {
       setPasswordError('비밀번호를 8~16자리 사이로 입력해주세요.')
-      window.scrollTo(100, 500)
+      window.scrollTo(0, 0)
     }
     if (post === '') {
       setPostError('제목을 입력해주세요.')
-      window.scrollTo(100, 500)
+      window.scrollTo(0, 200)
     }
     if (content === '') {
       setContentError('내용을 입력해 주세요.')
-      window.scrollTo(100, 500)
+      window.scrollTo(0, 400)
     }
     if (
       writer !== '' &&
